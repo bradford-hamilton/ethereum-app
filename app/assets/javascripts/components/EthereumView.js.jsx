e896c32483045adca4b14ecf2fb1d518204e52a2
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-09-12 16:30:47 -0600

    [feature] create ethereum route

---
+++ app/assets/javascripts/components/EthereumView.js.jsx
@@ -0,0 +1,13 @@
+class EthereumView extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ethereum prices</h1>
+      </div>
+    )
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  get 'ethereum' => 'ethereum#index'
 end
